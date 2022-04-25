234631b90098f682e8e4dc7ad3199a8088800411
Harushi1710 <hninh1710@gmail.com>
2022-04-25 17:03:52 +0700

    fuk @react-navigation

---
+++ src/screens/PlayListDetail.jsx
@@ -0,0 +1,49 @@
+import AudioListItem from "../components/AudioListItem";
+import { FlatList, Modal, Text, View } from 'react-native'
+import tw from 'twrnc'
+import {selectAudio} from "../misc/audioController";
+import React, {useContext} from "react";
+import {AudioContext} from "../context/AudioProvider";
+
+function PlaylistDetail(props) {
+    const { visible, playlist, onClose } = props
+
+    const context = useContext(AudioContext)
+    const playAudio = (audio) => {
+        selectAudio(audio, context)
+    }
+    return (
+        <Modal
+            visible={visible}
+            animationType="slide"
+            transparent
+            onRequestClose={onClose}
+        >
+            <View
+                style={tw`bg-neutral-400 z-50 h-96 absolute bottom-0 left-0 right-0 rounded-t-3xl pt-4 mx-2`}
+            >
+                <Text
+                    style={tw`text-white text-center ml-4 capitalize text-3xl font-bold`}
+                >
+                    {playlist.title}
+                </Text>
+                <FlatList
+                    data={playlist.audios}
+                    style={tw`mx-4`}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <AudioListItem
+                            {...{
+                                name: item.filename,
+                                duration: item.duration,
+                                onPress: () => playAudio(item)
+                            }}
+                        />
+                    )}
+                />
+            </View>
+        </Modal>
+    )
+}
+
+export default PlaylistDetail
