d2a23a4cf902e317998bf97d989c6b0cc8b28db0
Nyam <namnv.forwork@gmail.com>
2022-06-05 20:12:55 +0700

    Update services.js

--- src/helpers/services.js
+++ src/helpers/services.js
@@ -1,21 +1,21 @@
 import { LOCAL_URL } from '@env'
 
 // don't know why things are not working without this line so don't delete it :)
-console.log("LOCAL_URL: ", LOCAL_URL)
+console.log('LOCAL_URL: ', LOCAL_URL)
 
 export const authentication = async (props) => {
   const { action, data } = props
   try {
     const response = await fetch(`${LOCAL_URL}/api/auth/${action}`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     }).then((res) => res.json())
     return response
   } catch (error) {
-    console.log("Error inside authentication: ", error)
+    console.log('Error inside authentication: ', error)
   }
 }
 
@@ -23,18 +23,18 @@ export const zing = async (props) => {
   const { action, param } = props
   try {
     const response = await fetch(
-      `http://localhost:5000/api/zing/${action}?${
-        action === "search" ? "keyword" : "id"
+      `${LOCAL_URL}/api/zing/${action}?${
+        action === 'search' ? 'keyword' : 'id'
       }=${param}`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       }
     ).then((res) => res.json())
     return response
   } catch (error) {
-    console.log("Error inside zing: ", error)
+    console.log('Error inside zing: ', error)
   }
 }
