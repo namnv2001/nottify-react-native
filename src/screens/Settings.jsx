9c27d02ec65d4c7aea77243cf14127aca8fa5fab
namnv2001 <namnv.forwork@gmail.com>
2022-06-05 21:09:43 +0700

    style and convention fix

--- src/screens/Settings.jsx
+++ src/screens/Settings.jsx
@@ -1,53 +1,50 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import tw from 'style/tailwind'
-import React, {useContext, useState} from 'react'
-import { authentication } from 'helpers/services'
 import { AppContext } from 'context/AppProvider'
-import Navigator from 'components/Navigator'
+import { authentication } from 'helpers/services'
+import React, { useContext, useState, useCallback } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'style/tailwind'
 
-function Settings({navigation}) {
+function Settings() {
+  const appContext = useContext(AppContext)
+  const data = { username: appContext.username }
 
-    const appContext = useContext(AppContext)
-    const data = { username: appContext.username}
+  const [logoutError, setLogoutError] = useState('')
 
-    const [logoutError, setLogoutError] = useState('')
-    
-    const handleLogout = React.useCallback(async () => {
-      
-      const res = await authentication({ action: 'logout', data })
-      try {
-        if (res.status === 401) {
-          setLogoutError(res.message)
-        } else {    
-          appContext.updateState(appContext, {
-            loggedIn: false,
-          })
-          navigation.navigate({ name: 'Login' })  
-        }
-      } catch (error) {
-        console.log(error.message)
-        setLogoutError = error.message
+  const handleLogout = useCallback(async () => {
+    const res = await authentication({ action: 'logout', data })
+    try {
+      if (res.status === 401) {
+        setLogoutError(res.message)
+      } else {
+        appContext.updateState(appContext, {
+          loggedIn: false,
+        })
       }
-    },[])
+    } catch (error) {
+      setLogoutError = error.message
+    }
+  }, [data])
 
   return (
-    <View style={tw`px-8 bg-secondary h-full justify-center `}>
-        <TouchableOpacity style={tw`mt-8 flex-row items-center bg-thirdly rounded-xl justify-center`}
-          onPress={handleLogout}>
-          <Text style={tw`text-lg p-4 px-4 text-white `}> Logout </Text>
-        </TouchableOpacity>
-        <Text style={styles.error}>{logoutError}</Text>
+    <View style={tw`px-8 bg-secondary h-full justify-center`}>
+      <TouchableOpacity
+        style={tw`mt-8 flex-row items-center bg-thirdly rounded-xl justify-center`}
+        onPress={handleLogout}
+      >
+        <Text style={tw`text-lg p-4 px-4 text-white`}>Logout </Text>
+      </TouchableOpacity>
+      {logoutError ? <Text style={styles.error}>{logoutError}</Text> : null}
     </View>
   )
 }
 
-  const styles = StyleSheet.create({
-    error: {
-      fontSize: 12,
-      color: 'red',
-      marginTop: 8,
-      textAlign: 'center'
-    }
-  })
+const styles = StyleSheet.create({
+  error: {
+    fontSize: 16,
+    color: 'red',
+    marginTop: 8,
+    textAlign: 'center',
+  },
+})
 
 export default Settings
